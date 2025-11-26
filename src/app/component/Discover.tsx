00c37a6c9ffa1303e8f0fbca9024ca4cd7218e73
Monika H J <monikahj7@gmail.com>
2025-11-26 21:49:23 +0530

    done

---
+++ src/app/component/Discover.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "../../app/styles/Discover.module.css";
+
+const Discover = () => {
+  return (
+    <div className={styles.discoverWrapper}>
+      <p className={styles.title}>DISCOVER OUR PRODUCTS</p>
+
+      <p className={styles.subtitle}>
+        Loream Logo text and menu should be center. Menu should be below LOGO
+        text .This is also
+         Loream Logo text and menu should be center. Menu should be below LOGO
+        text (stacked).
+      </p>
+    </div>
+  );
+};
+
+export default Discover;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "../app/component/Navbar"
+import Discover from "../app/component/Discover"
 
 export default function Home() {
   return (
     <div>
        <Navbar/>
+       <Discover/>
     </div>
   );
 }
