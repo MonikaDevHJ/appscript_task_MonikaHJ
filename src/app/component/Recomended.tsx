e34ddf2c45140348aaf076deb64d2b468d6ada59
Monika H J <monikahj7@gmail.com>
2025-11-27 10:57:14 +0530

    domne

--- src/app/component/Recomended.tsx
+++ src/app/component/Recomended.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+"use client";
 
-const Recomended = () => {
+import { useState } from "react";
+import styles from "../../app/styles/Recomended.module.css";
+
+export default function Recommended() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>
+    <div className={styles.wrapper}>
       
-    </div>
-  )
-}
+      {/* LEFT SIDE */}
+      <div className={styles.left}>
+        <span className={styles.itemsCount}>3425 ITEMS</span>
+        <button className={styles.hideFilterBtn}>HIDE FILTER</button>
+      </div>
 
-export default Recomended
+      {/* RIGHT SIDE */}
+      <div className={styles.right}>
+        <button
+          className={styles.dropdownButton}
+          onClick={() => setOpen(!open)}
+        >
+          Recommended
+          <span className={`${styles.arrow} ${open ? styles.open : ""}`}>▼</span>
+        </button>
+
+        {open && (
+          <div className={styles.dropdownMenu}>
+            <p>Recommended</p>
+            <p>Newest First</p>
+            <p>Popular</p>
+            <p>Price: High to Low</p>
+            <p>Price: Low to High</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
